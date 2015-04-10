11de5aae2760ca22d4fbe51f9ae5d8287ffdb13a
vigneshPrakasam <vigneshp1990@gmail.com>
2015-04-10 22:39:25 +0800

    pushing phase1 script

--- phase1/scrapIt/scrapIt/phase1.py
+++ phase1/scrapIt/scrapIt/phase1.py
@@ -3,76 +3,78 @@ __author__ = 'Vignesh Prakasam'
 import json, os
 from bs4 import BeautifulSoup
 
-
-def injectionsPoints():
-    rawGetDictionary = getParams()
-    allKeys = rawGetDictionary.keys()
-    allVals = rawGetDictionary.values()
-    jsonPhase1 = {}
-    eleJson = {}
-    i = 0
-    # print allKeys
-    # print allVals
-    tot = zip(allKeys, allVals)
-    for url, paramValue in tot:
-        # print url
-        # print paramValue
-        i = 0
-        eleJson["type"] = "get"
-        for val in paramValue:
-            eleJson["param"+str(i)] = val
-            i += 1
-        jsonPhase1[url] = eleJson
-        eleJson = {}
-    jsonString = json.dumps(jsonPhase1, sort_keys=True, indent=8)
-    print jsonString
-
-
-def getParams():
-    alreadyCrawled = {}
+def getParams(getParamsJson):
+    # alreadyCrawled = {}
+    # getParamsJson = {}
     filepath = os.path.dirname(os.path.abspath(__file__)).strip("\scrapIt")
-    filepath = filepath + "\scrapIt\getLinks.txt"
+    filepath = filepath + "\scrapIt\\newgetLinks.txt"
     with open(filepath, 'r') as f:
         content = f.readlines()
     for link in content:
+        nlink = link.split("\n")
+        link = nlink[0]
+        eleJson = {}
         if "?" in link:
             splitLink = link.split("?")
             # print splitLink[0]
             # print splitLink[1]
-            if alreadyCrawled.get(link) == splitLink[1]:
-                pass
-            else:
-                params = []
-                str1 = splitLink[1].split("=")
-                for p in str1:
-                    if "&" in p:
-                        str2 = p.split("&")
-                        params.append(str2[1])
-                    else:
-                        params.append(p)
-                params.pop()
-                alreadyCrawled[link] = params
-            splitLink = []
+            str1 = splitLink[1].split("&")
+            for parList in str1:
+                param, value = parList.split("=")
+                eleJson[param] = value
+            eleJson["type"] = "get"
+            getParamsJson[splitLink[0]] = eleJson
+    # jsonString = json.dumps(getParamsJson, sort_keys=True, indent=8)
+    # print jsonString
+    return getParamsJson
 
-    return alreadyCrawled
+def postParams(phase1json):
+    postParamsJson = phase1json
+    filepath = os.path.dirname(os.path.abspath(__file__)).strip("\scrapIt")
+    filepath = filepath + "\scrapIt\\app2Check.txt"
+    with open(filepath, 'r') as f:
+        content = f.read()
+    soup = BeautifulSoup(content)
+    forms = soup.find_all('form')
+    for form in forms:
+        eleJson = {}
+        if form.get('method') is not None:
+            if form.get('method').lower() == "post":
+                print "******************POST*******************************************"
+                i = 0
+                print form.get('action')
+                formSoup = BeautifulSoup(str(form))
+                inpTag = formSoup.find_all('input')
+                for inp in inpTag:
+                    if inp.get('type') == "hidden" or inp.get('type') == "text":
+                        print inp.get('name'), " = ", inp.get('value')
+                        eleJson['type'] = "post"
+                        # eleJson["param"+str(i)] = inp.get('name')
+                        eleJson[inp.get('name')] = inp.get('value')
+                        i += 1
+                postParamsJson[form.get('action')] = eleJson
 
+            if form.get('method').lower() == "get":
+                print "*********************GET*****************************************"
+                i = 0
+                print form.get('action')
+                formSoup = BeautifulSoup(str(form))
+                inpTag = formSoup.find_all('input')
+                for inp in inpTag:
+                    if inp.get('type') == "hidden" or inp.get('type') == "text":
+                        # print inp.get('name'), " = ", inp.get('value')
+                        eleJson['type'] = "get"
+                        # eleJson["param"+str(i)] = inp.get('name')
+                        eleJson[inp.get('name')] = inp.get('value')
+                        i += 1
+                postParamsJson[form.get('action')] = eleJson
+    # jsonString = json.dumps(postParamsJson, sort_keys=True, indent=8)
+    # print jsonString
+    return postParamsJson
 
+json1 = {}
+json2 = getParams(json1)
+phase1json = postParams(json2)
+jsonString = json.dumps(phase1json, sort_keys=True, indent=8)
+print jsonString
 # injectionsPoints()
-# filepath = os.path.dirname(os.path.abspath(__file__)).strip("\scrapIt")
-# print filepath+"\scrapIt\getLinks.txt"
-filepath1 = os.path.dirname(os.path.abspath(__file__)).strip("\scrapIt")
-filepath1 = filepath1 + "\scrapIt\\app2Check.txt"
-with open(filepath1, 'r') as f:
-    content = f.read()
-soup = BeautifulSoup(content)
-forms = soup.find_all('form')
-filepath2 = os.path.dirname(os.path.abspath(__file__)).strip("\scrapIt")
-filepath2 = filepath2 + "\scrapIt\\check.txt"
-for form in forms:
-    # with open(filepath2, 'a') as f:
-    #     f.write(str(form))
-    print form.get('action')
-    formSoup = BeautifulSoup(str(form))
-    inpTag = formSoup.find_all('input')
-    for inp in inpTag:
-        print inp.get('name'), " = ", inp.get('value')
--- phase1/scrapIt/scrapIt/spiders/dom_spider.py
+++ phase1/scrapIt/scrapIt/spiders/dom_spider.py
@@ -6,6 +6,7 @@ import scrapy
 from scrapy.http import FormRequest
 from scrapy import log
 from scrapy.exceptions import DropItem
+from bs4 import BeautifulSoup
 
 class ExampleSpider(CrawlSpider):
     name = 'app2'
@@ -21,6 +22,13 @@ class ExampleSpider(CrawlSpider):
     def parse_page(self, response):
         """ Scrape useful stuff from page, and spawn new requests
         """
+        ############################################
+        soup = BeautifulSoup(response.body)
+        forms = soup.find_all('form')
+        with open("app2Check.txt", 'a') as f:
+            f.write(str(forms))
+        ############################################
+
         duplicateCheck = {}
         hxs = HtmlXPathSelector(response)
         links = hxs.select('//a/@href').extract()
@@ -34,6 +42,7 @@ class ExampleSpider(CrawlSpider):
                 with open("newpostLinks.txt", 'a') as f:
                     f.write(postLink+"\n")
 
+
         for link in links:
             # print link
             #only process external/full link
@@ -44,44 +53,45 @@ class ExampleSpider(CrawlSpider):
                         f.write(link+"\n")
                     yield Request(url=link, callback=self.parse_page)
 
-#independent checks
-class AppSpider(CrawlSpider):
-    name = 'app2-check'
-    start_urls = ['https://app2.com/login/index.php']
 
-    # 'log' and 'pwd' are names of the username and password fields
-    # depends on each website, you'll have to change those fields properly
-    # one may use loginform lib https://github.com/scrapy/loginform to make it easier
-    # when handling multiple credentials from multiple sites.
-    def parse(self, response):
-        return FormRequest.from_response(
-            response,
-            formdata={'username': 'admin', 'password': 'AdminAdmin1!'},
-            callback=self.after_login
-        )
-
-
-    def after_login(self, response):
-        # check login succeed before going on
-        if "Invalid login" in response.body:
-            self.log("Login failed", level=log.ERROR)
-            return
-
-        # continue scraping with authenticated session...
-        else:
-            self.log("Login succeed!", level=log.DEBUG)
-            return Request(url="https://app2.com/backup/restorefile.php?contextid=20",
-                           callback=self.parse_page)
-
-    # example of crawling all other urls in the site with the same
-    # authenticated session.
-    def parse_page(self, response):
-        """ Scrape useful stuff from page, and spawn new requests
-        """
-        # hxs = HtmlXPathSelector(response)
-        # links = hxs.select('//a/@href').extract()
-        # print links
-        with open("app2Check.txt", 'w') as f:
-            f.write(response.body)
-            # for link in links:
-            #     f.write(link)
+# #independent checks
+# class AppSpider(CrawlSpider):
+#     name = 'app2-check'
+#     start_urls = ['https://app2.com/login/index.php']
+#
+#     # 'log' and 'pwd' are names of the username and password fields
+#     # depends on each website, you'll have to change those fields properly
+#     # one may use loginform lib https://github.com/scrapy/loginform to make it easier
+#     # when handling multiple credentials from multiple sites.
+#     def parse(self, response):
+#         return FormRequest.from_response(
+#             response,
+#             formdata={'username': 'admin', 'password': 'AdminAdmin1!'},
+#             callback=self.after_login
+#         )
+#
+#
+#     def after_login(self, response):
+#         # check login succeed before going on
+#         if "Invalid login" in response.body:
+#             self.log("Login failed", level=log.ERROR)
+#             return
+#
+#         # continue scraping with authenticated session...
+#         else:
+#             self.log("Login succeed!", level=log.DEBUG)
+#             return Request(url="https://app2.com/backup/restorefile.php?contextid=20",
+#                            callback=self.parse_page)
+#
+#     # example of crawling all other urls in the site with the same
+#     # authenticated session.
+#     def parse_page(self, response):
+#         """ Scrape useful stuff from page, and spawn new requests
+#         """
+#         # hxs = HtmlXPathSelector(response)
+#         # links = hxs.select('//a/@href').extract()
+#         # print links
+#         with open("app2Check.txt", 'w') as f:
+#             f.write(response.body)
+#             # for link in links:
+#             #     f.write(link)
